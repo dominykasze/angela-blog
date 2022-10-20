b147af94b41dc98095d4fe8b6ba12120652f1f1b
Dominykas <dominykas.zenkevicius@nordsec.com>
2022-10-20 17:51:23 +0300

    Move secret_key to config variable, update database URL and change requirements.xtxt

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# Moving the SECRET_KEY into the config variable
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -42,7 +43,8 @@ def load_user(user_id):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# Changing the database from SQLite to Postgres
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
